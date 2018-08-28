bd7e974b9813fe52a83c714023cab48455f0ce40
Dantesto <34121957+Dantesto@users.noreply.github.com>
2018-08-28 20:59:36 +0300

    Add files via upload

--- java/com/example/android/miwok/MainActivity.java
+++ java/com/example/android/miwok/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,54 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        makeNumbersTextView();
-    }
-
-    //Пример програмного переопределения метода onClick без использования данного атрибута в xml
-    private void makeNumbersTextView() {
-        TextView numbers = (TextView) findViewById(R.id.numbers);
-        //Меры безопасности
-        if (numbers != null) {
-            numbers.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    /*Здесь можно переопределить метод onClick, но т.к. я уже написал для него метод, я просто его же и вызвал.
-                    Если бы я просто указал атрибут onClick в xml файле, достаточно было бы просто написать метод openNumbersList,
-                    как я это сделал с остальными TextView
-                     */
-                    openNumbersList(findViewById(R.id.numbers));
-                }
-            });
-        }
-    }
-
-    public void openColorsList(View view) {
-        Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
-        startActivity(intent);
-    }
-
-    public void openFamilyList(View view) {
-        Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
-        startActivity(intent);
-    }
-
-    public void openNumbersList(View view) {
-        Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
-        startActivity(intent);
-    }
-
-    public void openPhrasesList(View view) {
-        Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
-        startActivity(intent);
-    }
-
-    public void openTestList(View view) {
-        Intent intent = new Intent(MainActivity.this, TestActivity.class);
-        startActivity(intent);
-    }
-
-    public void openTastyList(View view) {
-        Intent intent = new Intent(MainActivity.this, TastyActivity.class);
-        startActivity(intent);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
